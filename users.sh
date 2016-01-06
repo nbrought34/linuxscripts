575f5209fe793be850413c5e1a3f665bcad9e24d
Nick <nbrought34@southhills.edu>
2016-01-06 12:15:34 -0500

    today

---
+++ users.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+echo "Script starting now."
+echo
+echo "Hello $USER..."
+echo
+echo "I'll fetch the currently logged-in users:"
+echo
+w
+echo
+echo "You're on your own now. Goodbye."
+echo
+COLOR="red"
+echo "Just an FYI..."
+echo
+echo "my favorite color is $COLOR"
+echo
